0a28b5af104b0305ab5bda236e44b2f0746ab2ce
Martin Tillenius <martin.tillenius@gmail.com>
2022-12-03 22:45:22 +0100

    end game tools page

--- bets-slutspel1.js
+++ bets-slutspel1.js
@@ -27,3 +27,8 @@ gamblerListTDB.push(new Gambler("Stefan","Stefan","PAID",
 	new Array(2,2,1,0,0,3,0,1),
 	new Array(1,0,1,0,1,0,2,1),
 	new Array("1","1","X","X","2","1","2","X")));
+
+gamblerListTools.push(new Gambler("Leon","Leon","PAID",
+	new Array(2,2,1,2,0,3,0,2),
+	new Array(1,0,0,1,1,0,2,0),
+	new Array("1","1","1","1","2","1","2","1")));
