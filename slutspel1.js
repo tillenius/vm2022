1e4b698583e9fa044d711eba8d7bb09c6f4b68ca
Martin Tillenius <martin.tillenius@gmail.com>
2022-12-04 22:25:42 +0100

    fixed bad data

--- slutspel1.js
+++ slutspel1.js
@@ -16,9 +16,9 @@ function Game(dateandtime, home, away, homegoal, awaygoal, odds1, oddsx, odds2, 
 gameList = new Array();
 gameList.push(new Game("3 dec 16:00", "Nederländerna"         ,"USA"        , -1, -1, 0, 0, 0, 400128136, "NED","USA", 0 ));
 gameList.push(new Game("3 dec 20.00", "Argentina"             ,"Australien" , -1, -1, 0, 0, 0, 400128131, "ARG","AUS", 1 ));
-gameList.push(new Game("4 dec 16.00", "Frankrike"             ,"Polen"      , -1, -1, 0, 0, 0, 400128135, "FRA","POL", 1 ));
-gameList.push(new Game("4 dec 20.00", "England"               ,"Senegal"    , -1, -1, 0, 0, 0, 400128134, "ENG","SEN", 1 ));
-gameList.push(new Game("5 dec 16.00", "Japan"                 ,"Kroatien"   , -1, -1, 0, 0, 0, 400128132, "JPN","CRO", 1 ));
-gameList.push(new Game("5 dec 20.00", "Brasilien"             ,"Sydkorea"   , -1, -1, 0, 0, 0, 400128133, "BRA","KOR", 1 ));
-gameList.push(new Game("6 dec 16.00", "Marocko"               ,"Spanien"    , -1, -1, 0, 0, 0, 400128137, "MAR","ESP", 1 ));
-gameList.push(new Game("6 dec 20.00", "Portugal"              ,"Schweiz"    , -1, -1, 0, 0, 0, 400128130, "POR","SUI", 1 ));
+gameList.push(new Game("4 dec 16.00", "Frankrike"             ,"Polen"      , -1, -1, 0, 0, 0, 400128135, "FRA","POL", 2 ));
+gameList.push(new Game("4 dec 20.00", "England"               ,"Senegal"    , -1, -1, 0, 0, 0, 400128134, "ENG","SEN", 3 ));
+gameList.push(new Game("5 dec 16.00", "Japan"                 ,"Kroatien"   , -1, -1, 0, 0, 0, 400128132, "JPN","CRO", 4 ));
+gameList.push(new Game("5 dec 20.00", "Brasilien"             ,"Sydkorea"   , -1, -1, 0, 0, 0, 400128133, "BRA","KOR", 5 ));
+gameList.push(new Game("6 dec 16.00", "Marocko"               ,"Spanien"    , -1, -1, 0, 0, 0, 400128137, "MAR","ESP", 6 ));
+gameList.push(new Game("6 dec 20.00", "Portugal"              ,"Schweiz"    , -1, -1, 0, 0, 0, 400128130, "POR","SUI", 7 ));
