8f33e527acaf1ff00b870a4bea80a0b9a0184d16
Martin Tillenius <martin.tillenius@gmail.com>
2022-12-10 13:09:51 +0100

    more bets

--- bets-slutspel1.js
+++ bets-slutspel1.js
@@ -4,34 +4,34 @@ gamblerList.push(new Gambler("Martin","Martin","PAID",
 	new Array("X","1","1","1","2","1","2","2","2","X","2","X")));
 
 gamblerList.push(new Gambler("Jens","Jens","PAID",
-	new Array(1,0,0,0,1,0,0,1),
-	new Array(0,0,1,0,0,1,1,1),
-	new Array("1","X","2","X","1","2","2","X")));
+	new Array(1,0,0,0,1,0,0,1,2,2,1,0),
+	new Array(0,0,1,0,0,1,1,1,1,0,0,0),
+	new Array("1","X","2","X","1","2","2","X","1","1","1","X")));
 
 gamblerListTDB.push(new Gambler("ergh","ergh","PAID",
-	new Array(1,2,2,3,2,2,0,3),
-	new Array(0,1,0,2,3,1,2,2),
-	new Array("1","1","1","1","2","1","2","1")));
+	new Array(1,2,2,3,2,2,0,3,1,2,0,2),
+	new Array(0,1,0,2,3,1,2,2,3,1,3,3),
+	new Array("1","1","1","1","2","1","2","1","2","1","2","2")));
 
 gamblerListTDB.push(new Gambler("Sverek","Sverek","PAID",
-	new Array(1,3,2,1,1,3,1,2),
-	new Array(1,1,1,0,0,0,1,1),
-	new Array("X","1","1","1","1","1","X","1")));
+	new Array(1,3,2,1,1,3,1,2,1,1,0,1),
+	new Array(1,1,1,0,0,0,1,1,2,1,3,1),
+	new Array("X","1","1","1","1","1","X","1","2","X","2","X")));
 
 gamblerListTDB.push(new Gambler("Magnus","Magnus","PAID",
-	new Array(1,1,2,2,2,3,1,2),
-	new Array(1,0,0,1,2,2,2,2),
-	new Array("X","1","1","1","X","1","2","X")));
+	new Array(1,1,2,2,2,3,1,2,1,3,0,2),
+	new Array(1,0,0,1,2,2,2,2,3,0,0,1),
+	new Array("X","1","1","1","X","1","2","X","2","1","X","1")));
 
 gamblerListTDB.push(new Gambler("Stefan","Stefan","PAID",
-	new Array(2,2,1,0,0,3,0,1),
-	new Array(1,0,1,0,1,0,2,1),
-	new Array("1","1","X","X","2","1","2","X")));
+	new Array(2,2,1,0,0,3,0,1,1,2,1,1),
+	new Array(1,0,1,0,1,0,2,1,2,2,3,1),
+	new Array("1","1","X","X","2","1","2","X","2","X","2","X")));
 
 gamblerListTools.push(new Gambler("Leon","Leon","PAID",
-	new Array(2,2,1,2,0,3,0,2),
-	new Array(1,0,0,1,1,0,2,0),
-	new Array("1","1","1","1","2","1","2","1")));
+	new Array(2,2,1,2,0,3,0,2,0,2,1,1),
+	new Array(1,0,0,1,1,0,2,0,2,1,2,1),
+	new Array("1","1","1","1","2","1","2","1","2","1","2","X")));
 
 gamblerListTools.push(new Gambler("Mikael","Mikael","PAID",
 	new Array(1,3,2,2,2,1,0,1),
