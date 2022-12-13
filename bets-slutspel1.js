ba0090a6d03396ce6b5e43b6bed4cb8388908066
Martin Tillenius <martin.tillenius@gmail.com>
2022-12-13 16:14:08 +0100

    more bets

--- bets-slutspel1.js
+++ bets-slutspel1.js
@@ -34,6 +34,6 @@ gamblerListTools.push(new Gambler("Leon","Leon","PAID",
 	new Array("1","1","1","1","2","1","2","1","2","1","2","X","1","1")));
 
 gamblerListTools.push(new Gambler("Mikael","Mikael","PAID",
-	new Array(1,3,2,2,2,1,0,1,0,1,0,1),
-	new Array(0,1,0,1,1,1,2,1,2,1,2,1),
-	new Array("1","1","1","1","1","X","2","X","2","X","2","X")));
+	new Array(1,3,2,2,2,1,0,1,0,1,0,1,1,2),
+	new Array(0,1,0,1,1,1,2,1,2,1,2,1,1,0),
+	new Array("1","1","1","1","1","X","2","X","2","X","2","X","X","1")));
