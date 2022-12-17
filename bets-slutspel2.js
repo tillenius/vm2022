8ebf82527a9335dae021971c5efe8530e52b8bb6
Martin Tillenius <martin.tillenius@gmail.com>
2022-12-17 22:57:40 +0100

    final tool bets

---
+++ bets-slutspel2.js
@@ -0,0 +1,19 @@
+gamblerList.push(new Gambler("Martin","Martin","PAID",
+	new Array(0,2,1,2,0,2,0,0,1,1,0,1,2,2,1),
+	new Array(0,0,0,0,2,0,2,1,3,1,2,1,0,1,1),
+	new Array("X","1","1","1","2","1","2","2","2","X","2","X","1","1","X")));
+
+gamblerList.push(new Gambler("Jens","Jens","PAID",
+	new Array(1,0,0,0,1,0,0,1,2,2,1,0,0,0,1),
+	new Array(0,0,1,0,0,1,1,1,1,0,0,0,1,0,2),
+	new Array("1","X","2","X","1","2","2","X","1","1","1","X","2","X","2")));
+
+gamblerListTools.push(new Gambler("Leon","Leon","PAID",
+	new Array(2,2,1,2,0,3,0,2,0,2,1,1,2,2,2),
+	new Array(1,0,0,1,1,0,2,0,2,1,2,1,1,0,1),
+	new Array("1","1","1","1","2","1","2","1","2","1","2","X","1","1","1")));
+
+gamblerListTools.push(new Gambler("Mikael","Mikael","PAID",
+	new Array(1,3,2,2,2,1,0,1,0,1,0,1,1,2,1),
+	new Array(0,1,0,1,1,1,2,1,2,1,2,1,1,0,0),
+	new Array("1","1","1","1","1","X","2","X","2","X","2","X","X","1","1")));
